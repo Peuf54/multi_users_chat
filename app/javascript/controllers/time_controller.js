0223f868340b3accc435e8db87a43d5091ff2364
Hugo Petament <h.petament@icloud.com>
2023-10-30 18:16:34 +0100

    improved time system

--- app/javascript/controllers/time_controller.js
+++ app/javascript/controllers/time_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="time"
 export default class extends Controller {
   static targets = ["time"];
   static values = { createdAt: Number };
@@ -16,42 +15,52 @@ export default class extends Controller {
 
   updateTime() {
     const now = new Date();
-    const createdAt = new Date(this.createdAtValue * 1000); // Convertir en millisecondes
-    const timeElement = this.timeTarget;
+    const createdAt = new Date(this.createdAtValue * 1000);
+    this.timeTarget.textContent = this.formatTimeDifference(now, createdAt);
+  }
 
+  formatTimeDifference(now, createdAt) {
     const diffInSeconds = (now - createdAt) / 1000;
     const diffInMinutes = diffInSeconds / 60;
-    const diffInHours = diffInMinutes / 60;
-    const diffInDays = diffInHours / 24;
+    const hours = createdAt.getHours().toString().padStart(2, '0');
+    const minutes = createdAt.getMinutes().toString().padStart(2, '0');
+    const month = createdAt.getMonth() + 1;
+    const day = createdAt.getDate();
+    const year = createdAt.getFullYear();
 
-    if (diffInSeconds < 1) { // 0 seconde
-      timeElement.textContent = `à l'instant`;
-    } else if (diffInSeconds < 60) { // moins d'une minute
-      timeElement.textContent = `il y a ${Math.round(diffInSeconds)} secondes`;
-    } else if (diffInMinutes < 2) { // moins de 2 minutes
-      timeElement.textContent = `il y a 1 minute`;
-    } else if (diffInMinutes < 10) { // moins de 10 minutes
-      timeElement.textContent = `il y a ${Math.round(diffInSeconds / 60)} minutes`;
-    } else if (diffInDays < 1) { // moins de 24 heures {
-      const hours = createdAt.getHours().toString().padStart(2, '0');
-      const minutes = createdAt.getMinutes().toString().padStart(2, '0');
-      timeElement.textContent = `${hours}:${minutes}`;
-    } else if (diffInDays < 2) { // moins de 2 jours
-      timeElement.textContent = `hier à ${createdAt.getHours()}:${createdAt.getMinutes()}`;
-    } else if (diffInDays < 30) { // moins de 30 jours
-      timeElement.textContent = `il y a ${Math.round(diffInDays)} jours`;
+    // Réinitialiser le temps pour les deux dates à minuit
+    const nowAtMidnight = new Date(now);
+    nowAtMidnight.setHours(0, 0, 0, 0);
+    const createdAtMidnight = new Date(createdAt);
+    createdAtMidnight.setHours(0, 0, 0, 0);
+
+    // Calculer la différence en jours entre aujourd'hui et la date de création
+    const diffInDaysFromMidnight = Math.round((nowAtMidnight - createdAtMidnight) / (1000 * 60 * 60 * 24));
+
+    if (diffInSeconds < 1) {
+      return `à l'instant`;
+    } else if (diffInSeconds < 60) {
+      return `il y a ${Math.round(diffInSeconds)} secondes`;
+    } else if (diffInMinutes < 2) {
+      return `il y a 1 minute`;
+    } else if (diffInMinutes < 60) {
+      return `il y a ${Math.round(diffInMinutes)} minutes`;
+    } else if (diffInDaysFromMidnight === 0) {
+      return `${hours}:${minutes}`;
+    } else if (diffInDaysFromMidnight === 1) {
+      return `hier à ${hours}:${minutes}`;
+    } else if (diffInDaysFromMidnight === 2) {
+      return `avant-hier à ${hours}:${minutes}`;
+    } else if (diffInDaysFromMidnight < 30) {
+      return `il y a ${Math.round(diffInDaysFromMidnight)} jours`;
     } else {
-      const month = createdAt.getMonth() + 1;
-      const day = createdAt.getDate();
-      const year = createdAt.getFullYear();
-      timeElement.textContent = `le ${day}/${month}/${year}`;
+      return `le ${day}/${month}/${year}`;
     }
   }
 
+
   startRefreshing() {
-    this.refreshInterval = setInterval(() => {
-      this.updateTime();
-    }, 10000);
+    this.refreshInterval = setInterval(() => this.updateTime(), 10000);
   }
 
   stopRefreshing() {
